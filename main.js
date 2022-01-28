56cc7c0033421d5996b5ae8a11f270cdbcc39f53
Petr Bucek <p_bucek@utb.cz>
2022-01-28 14:40:22 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,62 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// SCENE, CAMERA AND RENDERER SETUP
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, .1, 1000)
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#webgl')
+})
+renderer.setPixelRatio(window.devicePixelRatio)
+renderer.setSize(window.innerWidth, window.innerHeight)
+camera.position.setZ(5)
+
+// OBJECTS
+const geometry = new THREE.TorusGeometry(1, .3, 16, 100)
+
+// MATERIALS
+const material = new THREE.MeshBasicMaterial({color: 0x00aaff})
+
+// MESH
+const torus = new THREE.Mesh(geometry, material)
+scene.add(torus)
+
+// LIGHTS
+const pointLight = new THREE.PointLight(0xffffff)
+const ambientLight = new THREE.AmbientLight(0xffffff)
+pointLight.position.set(2,3,4)
+scene.add(pointLight, ambientLight)
+
+// SIZES
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight
+}
+window.addEventListener('resize', () =>{
+  // UPDATE SIZES
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // UPDATE CAMERA
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // UPDATE RENDERER
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(window.devicePixelRatio)
+})
+
+// CONTROLS
+const controls = new OrbitControls(camera, renderer.domElement)
+
+// ANIMATE FUNCTION
+function animate()
+{
+  torus.rotation.x += .025
+  torus.rotation.y += .025
+  
+  renderer.render(scene, camera)
+  requestAnimationFrame(animate)
+}
+animate()
